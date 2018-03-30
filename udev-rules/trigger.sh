bb695c7e9561265878e4a216f421d66772e2eb2f
zacharyasmith <zach@medialusions.com>
2018-03-30 11:17:58 -0600

    Update.

--- udev-rules/trigger.sh
+++ udev-rules/trigger.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
 mkdir -p /dev/udev/rules.d/
-cp -f 99-usb-serial.rules /dev/udev/rules.d/
+cp -f rs232.rules /dev/udev/rules.d/
+cp -f rs485.rules /dev/udev/rules.d/
 udevadm trigger
