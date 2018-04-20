94a26b01f422512e60769c41f8837beb6f6c7691
zacharyasmith <zach@medialusions.com>
2018-04-20 14:24:24 -0600

    LD2100 test

--- components/LD2100Tester.py
+++ components/LD2100Tester.py
@@ -39,10 +39,10 @@ class LD2100Tester(LDBoard):
                 self.process_test_result('datetime_set', ld_board.test_datetime_set())
                 self.process_test_result('startup_sequence', ld_board.test_startup_sequence())
                 self.process_test_result('ps_voltage', ld_board.test_voltage())
-                self.process_test_result('rs485_modbus', ld_board.test_modbus(num_ports=1))
-                self.process_test_result('ethernet_ping',
-                                         ld_board.test_ethernet(ip_address,
-                                                                configure_ip_address=True))
+                self.process_test_result('rs485_modbus', ld_board.test_modbus(LDBoardTester.LD2100))
+                # self.process_test_result('ethernet_ping',
+                #                          ld_board.test_ethernet(ip_address,
+                #                                                 configure_ip_address=True))
                 _LOGGER.info('Sleeping for 3 seconds...')
                 sleep(3)
                 self.process_test_result('datetime_read', ld_board.test_datetime_read())
