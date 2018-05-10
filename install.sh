2428ead6539466dbc07611aebbdf1088a08f9b0b
zacharyasmith <zach@medialusions.com>
2018-05-10 11:12:59 -0600

    Include icon

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -rf /opt/SeaLion
 mkdir /opt/SeaLion
-cp -R main.py RunMainWindow.py __init__.py components view /opt/SeaLion
+cp -R DesktopIcon.png main.py RunMainWindow.py __init__.py components view /opt/SeaLion
 cp SeaLion.desktop /usr/share/applications
